19a01d9267e83345a32670316d5f448424b19548
ningjinbiao <ningj4@mcmaster.ca>
2017-07-27 19:02:45 -0400

    store SLAM TEST

---
+++ SLAM_test/CMakeFiles/slam_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slam_test.dir/main.cpp.o"
+  "slam_test.pdb"
+  "slam_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slam_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
